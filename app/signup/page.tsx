bbd0ec3c0cedccd8865a043568f87b7801d82103
itrodi <oyetunjiinioluwajesu@gmail.com>
2025-05-27 05:52:49 +0100

    fast

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,4 @@
-import { SignUpForm } from "@/components/auth/sign-up-form"
-import { createClient } from "@/lib/supabase/server"
+
 import { redirect } from "next/navigation"
 
 export default async function SignUpPage() {
